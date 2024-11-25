0194af73a414275645df63e466676bc2e028cc25
gulash99 <daria.i.gulyaeva@gmail.com>
2024-11-25 20:32:45 +0300

    more tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -24,7 +24,12 @@ def test_bulls_and_cows(secret_number, guess, expected_bulls, expected_cows):
     "guess, expected_exception",
     [
         ("123", ValueError),  # Слишком короткое число
-        ("1123", ValueError),  # Повторяющиеся цифры
+        ("12345", ValueError),  # Слишком длинное число
+        ("1123", ValueError),  # Повторяющиеся цифры в числе
+        ("abcd", ValueError),  # Буквы вместо ожидаемых цифр
+        ("+-!!", ValueError),  # Символы вместо ожидаемых цифр
+        ("1!a4", ValueError),  # Смешение цифр, букв и символов
+        ("", ValueError),  # Пустая строка
     ]
 )
 def test_invalid_input(guess, expected_exception):
@@ -41,6 +46,7 @@ def test_generate_number():
     assert len(generated_number) == 4, "Число должно быть длиной 4"
     assert len(set(generated_number)) == len(generated_number), \
         "Цифры в числе должны быть уникальными"
+    # assert generated_number.isdigit(), "Число должно содержать только цифры"
 
 
 def test_gameplay():
@@ -63,7 +69,7 @@ def test_gameplay():
         assert bulls == expected_bulls, f"Для {guess}: ожидалось {expected_bulls} Б"
         assert cows == expected_cows, f"Для {guess}: ожидалось {expected_cows} К"
 
-    assert game.secret_number == "4567", "Секр.число должно остаться неизменным"
+    assert game.secret_number == "4567", "Секр.число должно быть неизменным"
 
 
 def test_game_over():
@@ -79,3 +85,18 @@ def test_game_over():
 
     assert bulls == 4, "Должно быть 4 быка"
     assert cows == 0, "Коров должно быть 0"
+
+
+# Тесты на мах/мин длину числа
+@pytest.mark.parametrize("length", [1, 2, 9, 10])
+def test_generate_large_numbers(length):
+    """
+    Тестируем генерацию чисел с минимальной и максимальной длиной.
+    Проверяем корректность и уникальность цифр.
+    """
+    game = BullsAndCows(length=length)
+    generated_number = game._generate_number()
+    assert len(generated_number) == length, f"Число должно быть длиной {length}"
+    assert len(set(generated_number)) == len(generated_number), \
+        "Цифры в числе должны быть уникальными"
+    assert generated_number.isdigit(), "Число должно содержать только цифры"
